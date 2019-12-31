8252b41aedb7adf970dc8c4a32c90ee8075f655b
MTQMcCarthy <mtqmccarthy@gmail.com>
2019-12-31 16:06:40 -0600

    finished exercises

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,27 +4,68 @@ import csv
 
 #load the file I need
 csvpath = os.path.join("Resources", "budget_data.csv")
-#pathout = os.path.join("Resources", "budget_analysis.txt")
 
-#Variables and values so I can track them 
+#create variables
 totalMonths = 0
 totalProfitLoss = 0
-avgProfitLoss = 0
-greatestIncrease = ["",0]
-greatestDecrease = ["",0]
-#revenue_change = 0
-#revenue_change_list = []
-#month_of_change = []
-#greatestDecrease = ["", 99999999999]
+pastProfitLoss = 0
+greatestIncrease = 0
+greatestDecrease = 99999999999
 
-#Read the budget_data.csv file
-with open(csvpath) as budgetData:
-    reader = csv.DictReader(budgetData)
-
-#calculate the total number of months included in the dataset
-    for row in reader:
-        
-        #count total months in csv file
+#create lists to store revenue change
+profitChange = []
+with open(csvpath, newline='', encoding='utf-8') as csvfile:
+    budget_csvreader = csv.reader(csvfile, delimiter=',')
+    next(budget_csvreader, None)
+    
+    for row in budget_csvreader:
+        #determine total months in file
         totalMonths = totalMonths + 1
+        
+        #determine total porifts/losses
+        totalProfitLoss = totalProfitLoss + (int(row[1]))
+        
+        #create a variable that will tally the change
+        monthlyProfitChange = int(row[1]) - pastProfitLoss
+        pastProfitLoss = int(row[1])
+        
+        #create a list to store the changes 
+        profitChange.append(monthlyProfitChange)
+        
+        #determine the average change in revenue
+        avgprofitChange = round(sum(profitChange)/totalMonths)
+        
+        #determine the greatest increase in revenue
+        if (monthlyProfitChange > greatestIncrease):
+            greatestIncreaseMonth = row[0]
+            greatestIncrease = monthlyProfitChange 
+        
+        #determine the greatest decrease in revenue
+        if (monthlyProfitChange < greatestDecrease):
+            greatestDecreaseMonth = row[0]
+            greatestDecrease = monthlyProfitChange
 
-print("Total Months: {totalMonth}\n")
+#varible to hold results and use f-strings for formatting
+Results = (
+f"Financial analysis\n"
+f"------------------\n"
+f"Total months: {totalMonths}\n"
+f"Total profits or losses: ${totalProfitLoss:,}\n"
+f"Average change in profits or losses: ${avgprofitChange:,}\n"
+f"Greatest increase in profits was ${greatestIncrease:,} seen in {greatestIncreaseMonth}\n"
+f"Greatest decrease in profits was ${greatestDecrease:,} seen in {greatestDecreaseMonth}\n")
+print(Results)
+
+# Set variable for output file
+pathout = os.path.join("Resources", "results.txt")
+
+#  Open the output file
+with open(pathout, "w", newline="") as datafile:
+    writer = csv.writer(datafile)
+
+    # Write the header row
+    writer.writerow(["Total months", "Total profits", "Average Change", "Greatest Increase",
+                     "Greatest Decrease"])
+
+    # Write in results
+    writer.writerows(Results)
---
+++ PyPoll/main.py
@@ -0,0 +1,54 @@
+#import modules
+import os
+import csv
+
+#load the file I need
+csvpath = os.path.join("Resources", "election_data.csv")
+#The total number of votes cast, list of candidates
+
+#create variables
+totalVotes = 0
+uniqueCandidate = {}
+candidatePercent = {}
+winnerCount = 0
+winner = ""
+
+with open(csvpath, 'r', newline='', encoding='utf-8') as csvfile:
+    election_csvreader = csv.DictReader(csvfile, delimiter=',')
+    next(election_csvreader, None)
+
+    for row in election_csvreader:
+        #determine the total votes
+        totalVotes += 1
+        #identify the list of unique candidates and tally
+        if row["Candidate"] in uniqueCandidate.keys():
+            uniqueCandidate[row["Candidate"]] += 1
+        else:
+            uniqueCandidate[row["Candidate"]] = 1
+
+        # percentages for each candidate by pulling info from items in the candidates dictionary
+        for key, value in uniqueCandidate.items():
+            candidatePercent[key] = round((value/totalVotes) * 100, 1)
+
+        # Finding the winner using the candidates dictionary. 
+        for key in uniqueCandidate.keys():
+            if uniqueCandidate[key] > winnerCount:
+                winner = key
+                winnerCount = uniqueCandidate[key]
+
+print("Election Results")
+print("-------------------------------------")
+print("Total Votes: " + str(totalVotes))
+print("-------------------------------------")
+for key, value in uniqueCandidate.items():
+    print(key + ": " + str(candidatePercent[key]) + "% (" + str(value) + ")")
+print("-------------------------------------")
+print("Winner: " + winner)
+print("-------------------------------------")
+
+
+print("Total Votes: " + str(totalVotes))
+#for key, value in candidates.items():
+print()
+
+
